900d6199eda2736d4aa4560cb7add1fe04c0f1bb
Sparsh Khandelwal <sparshkhandelwal0101@gmail.com>
2023-11-07 23:25:11 +0530

    Connection to the DB

--- api/index.js
+++ api/index.js
@@ -1,6 +1,11 @@
 import express from 'express';
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+mongoose.connect(process.env.MONGO_URL).then(() => console.log('MongoDB connected')).catch(err => console.log(err));
 
 
 
