e875dd1fbffc4e41aea8fa9a03178cce83e0c1ea
Sparsh Khandelwal <sparshkhandelwal0101@gmail.com>
2023-11-08 13:55:37 +0530

    signup-page with error and submit handler

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,16 +1,14 @@
-import bcrypt from 'bcrypt';
-import User from '../models/user.model.js';
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
-    //we will use bcrypt to hash the password
-    const hashPassword =  bcrypt.hashSync(password, 10);
-  const newUser = new User({ username, email, password: hashPassword });
-    try {
-        await newUser.save();
-        res.status(201).json(newUser);
-      }
-    catch (error) {
-        next(error);
-    }
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
+    await newUser.save();
+    res.status(201).json("User created successfully!");
+  } catch (error) {
+    next(error);
+  }
 };
--- api/utils/error.js
+++ api/utils/error.js
@@ -1,9 +1,8 @@
 export const errorHandler = (statusCode, message) => {
-    const error = new Error();
-    error.statusCode = statusCode;
-    error.message = message;
-    return error;
-}
+  const error = new Error();
+  error.statusCode = statusCode;
+  error.message = message;
+  return error;
+};  
 
-
-// this is the error handler middleware so what we do is we can manually assign the errors if we want to or we can use this function to assign the errors and then pass the error to the next middleware 
+// this is the error handler middleware so what we do is we can manually assign the errors if we want to or we can use this function to assign the errors and then pass the error to the next middleware
