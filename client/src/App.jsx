9b3fe6b1d1d04270f4f4cb27fd0462972487a161
Sparsh Khandelwal <sparshkhandelwal0101@gmail.com>
2023-11-09 00:36:17 +0530

    private rules

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,23 +6,25 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
 import Header from './components/Header';
+import PrivateRoute from "./components/PrivateRoute";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/about" element={<About />}/>
-        <Route path="/sign-in" element={<SignIn />}/>
-        <Route path="/sign-up" element={<SignUp />}/>
-        <Route path="/profile" element={<Profile />}/>
-        {/* <Route path="/home" element={<Home />}/> */}
-      </Routes>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
 export default App
